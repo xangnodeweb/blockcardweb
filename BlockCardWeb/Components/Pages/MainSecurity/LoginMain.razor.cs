4ef873fb9227e2ebdd71d7da583c93ab60d6f0c7
XangVk <xang.vk@gmail.com>
2024-11-07 13:54:08 +0700

    loginmain

--- BlockCardWeb/Components/Pages/MainSecurity/LoginMain.razor.cs
+++ BlockCardWeb/Components/Pages/MainSecurity/LoginMain.razor.cs
@@ -1,7 +1,36 @@
-﻿namespace BlockCardWeb.Components.Pages.MainSecurity
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace BlockCardWeb.Components.Pages.MainSecurity
 {
     public partial class LoginMain
     {
+        [Inject] public ILocalStorageService localStorage { get; set; }
+        [Inject] public NavigationManager nav { get; set; }
+
+
+
+        InputType passwordinput = InputType.Password;
+        public bool? isshow = false;
+        public string passwordinputicon = Icons.Material.Filled.VisibilityOff;
+
+
+        public void showpassword()
+        {
+            if (isshow == true)
+            {
+                isshow = false;
+                passwordinput = InputType.Password;
+                passwordinputicon = Icons.Material.Filled.VisibilityOff;
+            }
+            else
+            {
+                isshow = true;
+                passwordinput = InputType.Text;
+                passwordinputicon = Icons.Material.Filled.Visibility;
+            }
+        }
 
 
     }
