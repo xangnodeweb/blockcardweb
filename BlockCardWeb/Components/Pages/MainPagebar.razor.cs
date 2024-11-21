bd50b3d59266c5a27fe1707505af1f6a655ee041
XangVk <xang.vk@gmail.com>
2024-11-21 11:28:21 +0700

    update mainpage

---
+++ BlockCardWeb/Components/Pages/MainPagebar.razor.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace BlockCardWeb.Components.Pages
+{
+    public partial class MainPagebar
+    {
+
+        [Inject] public NavigationManager nav { get; set; }
+
+        public int? pathmain = 0;
+       
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            var path = nav.Uri;
+            var paths = nav.BaseUri.Split('/');
+            if (paths[3] == "")
+            {
+                pathmain = 0;
+            }
+            await InvokeAsync(StateHasChanged);
+        }
+
+
+    }
+}
